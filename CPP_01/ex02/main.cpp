1fe9102e5479e4619d6f879977202f908cb7da95
mathis <mathismartini1@gmail.com>
2022-08-30 16:26:12 +0200

    Remove const varaible

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -7,10 +7,10 @@
 
 int	main(void) {
 
-	std::string const brain = "HI THIS IS BRAIN";
+	std::string  brain = "HI THIS IS BRAIN";
 
-	std::string* const stringPTR = &brain;
-	std::string& const stringREF = brain;
+	std::string*  stringPTR = &brain;
+	std::string&  stringREF = brain;
 
 	std::cout << "string: " <<  &brain << " PTR: " << stringPTR << " REF: " << &stringREF << std::endl;
 	std::cout << "string value: " << brain << " PTR value: " << *stringPTR << " REF value:" << stringREF << std::endl;
